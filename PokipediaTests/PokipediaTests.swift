17d7409d354b04d4cae12cf5d13d82e577f6b151
Haoxin Li <54lihaoxin@gmail.com>
2018-07-01 17:17:02 -0700

    create empty project

---
+++ PokipediaTests/PokipediaTests.swift
@@ -0,0 +1,36 @@
+//
+//  PokipediaTests.swift
+//  PokipediaTests
+//
+//  Created by Haoxin Li on 7/1/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import XCTest
+@testable import Pokipedia
+
+class PokipediaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
